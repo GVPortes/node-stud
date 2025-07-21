db6d84166a525f88b50e7acd9b1ace6feb9d51dc
Gabriel Portes <gvportes@gmail.com>
2025-07-21 20:25:26 -0300

    feat: mais algumas coisinhas aprendidas 2.0

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ const port = 3000;
 app.get ('/', (req, res) => {
     console.log("Entrou aqui");
     res.send("Hello World");
+//ele nunca atualiza o console.log, ele só executa quando a rota é chamada
+// então se você chamar a rota várias vezes, ele vai imprimir várias vezes
+// mas o console.log só é executado quando a rota é chamada
+//para atualizar o console.log, você precisa reiniciar o servidor
+//mas agora a gente instalou o nodemon, então ele vai reiniciar automaticamente
+// então você não precisa mais reiniciar o servidor manualmente
+// o nodemon vai reiniciar o servidor automaticamente quando você salvar o arquivo
+//então você pode salvar o arquivo e ver o console.log atualizado
 });
 
 app.listen(port, () => {
